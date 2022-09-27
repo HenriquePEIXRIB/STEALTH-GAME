d96f2a1f590c8f90fb09c9d372448f75f4ba34e8
Henrique <peixoto.ribeiro8@gmail.com>
2022-09-27 11:00:01 +0200

    WIP

--- Assets/Script/CharactereMovement.cs
+++ Assets/Script/CharactereMovement.cs
@@ -8,13 +8,26 @@ public class CharactereMovement : MonoBehaviour
 {
     [SerializeField] InputActionReference _move;
     [SerializeField] Animator _animator;
+    [SerializeField] float _speed;
+    [SerializeField] Rigidbody _rb;
 
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
-        Vector2 value = _move.action.ReadValue<Vector2>();
+        Vector3 value = _move.action.ReadValue<Vector2>();
         float speed = value.magnitude;
         speed = Mathf.Clamp(value: speed, min: 0f, max: 1f);
         _animator.SetFloat("Speed", speed);
+
+        _rb.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        
+
+
+
     }
 }
